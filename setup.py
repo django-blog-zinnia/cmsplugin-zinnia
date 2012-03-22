65071bb4aeb99369032b89f098bc339c70d46805
Fantomas42 <fantomas42@gmail.com>
2012-03-22 17:18:55 +0100

    basic structure

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+import cmsplugin_zinnia
+
+setup(name='cmsplugin_zinnia',
+      version=cmsplugin_zinnia.__version__,
+
+      description='Django-CMS plugins for django-blog-zinnia',
+      long_description=open('README.rst').read(),
+
+      keywords='django, blog, weblog, zinnia, cms, plugins, apphook',
+
+      author=cmsplugin_zinnia.__author__,
+      author_email=cmsplugin_zinnia.__email__,
+      url=cmsplugin_zinnia.__url__,
+
+      packages=find_packages(exclude=['demo_cmsplugin_zinnia']),
+      classifiers=[
+          'Framework :: Django',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',],
+
+      license=cmsplugin_zinnia.__license__,
+      include_package_data=True,
+      zip_safe=False)
+
