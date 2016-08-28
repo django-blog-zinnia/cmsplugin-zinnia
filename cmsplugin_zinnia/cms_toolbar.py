b6c443f6c907765aa360ca198386b67b86bd56ad
Peter Bittner <django@bittner.it>
2016-08-28 17:10:54 +0200

    Disable menu items instead of removing them (when lacking permissions)

--- cmsplugin_zinnia/cms_toolbar.py
+++ cmsplugin_zinnia/cms_toolbar.py
@@ -13,31 +13,38 @@ class ZinniaToolbar(CMSToolbar):
         zinnia_menu = self.toolbar.get_or_create_menu(
             'zinnia-menu', _('Zinnia'))
 
-        if user.has_perm('zinnia.add_entry'):
-            url = reverse('admin:zinnia_entry_add')
-            zinnia_menu.add_sideframe_item(_('New entry'), url=url)
+        url = reverse('admin:zinnia_entry_add')
+        zinnia_menu.add_sideframe_item(
+            _('New entry'), url=url,
+            disabled=not user.has_perm('zinnia.add_entry'))
 
-        if user.has_perm('zinnia.add_category'):
-            url = reverse('admin:zinnia_category_add')
-            zinnia_menu.add_sideframe_item(_('New category'), url=url)
+        url = reverse('admin:zinnia_category_add')
+        zinnia_menu.add_sideframe_item(
+            _('New category'), url=url,
+            disabled=not user.has_perm('zinnia.add_category'))
 
-        if zinnia_menu.get_items():
-            zinnia_menu.add_break()
+        zinnia_menu.add_break()
 
-        if user.has_perm('zinnia.change_entry'):
-            url = reverse('admin:zinnia_entry_changelist')
-            zinnia_menu.add_sideframe_item(_('Entries list'), url=url)
+        url = reverse('admin:zinnia_entry_changelist')
+        zinnia_menu.add_sideframe_item(
+            _('Entries list'), url=url,
+            disabled=not user.has_perm('zinnia.change_entry'))
 
-        if user.has_perm('zinnia.change_category'):
-            url = reverse('admin:zinnia_category_changelist')
-            zinnia_menu.add_sideframe_item(_('Categories list'), url=url)
+        url = reverse('admin:zinnia_category_changelist')
+        zinnia_menu.add_sideframe_item(
+            _('Categories list'), url=url,
+            disabled=not user.has_perm('zinnia.change_category'))
 
-        if user.has_perm('tagging.change_tag'):
-            url = reverse('admin:tagging_tag_changelist')
-            zinnia_menu.add_sideframe_item(_('Tags list'), url=url)
+        url = reverse('admin:tagging_tag_changelist')
+        zinnia_menu.add_sideframe_item(
+            _('Tags list'), url=url,
+            disabled=not user.has_perm('tagging.change_tag'))
 
-        if not zinnia_menu.get_items():
-            self.toolbar.remove_item(zinnia_menu)
+        # remove complete menu if all items are disabled
+        for item in zinnia_menu.get_items():
+            if hasattr(item, 'disabled') and not item.disabled:
+                return
+        self.toolbar.remove_item(zinnia_menu)
 
 
 toolbar_pool.register(ZinniaToolbar)
